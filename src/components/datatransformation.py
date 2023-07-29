73a5f982cef1b2f74627b887aeecf403f22346fc
Syed Obaid Uddin <syed@Syeds-MacBook-Air.local>
2023-07-29 14:20:58 +0530

    file changes

--- src/components/datatransformation.py
+++ src/components/datatransformation.py
@@ -25,19 +25,16 @@ class Data_Transformation :
 
     def get_data_transformer_object(self):
         try :
-            # define custom func to replace "NA" with np.nan
-            replace_na_with_nan = lambda X : np.where(X == "na", np.nan, X)
-
+            
             # define the steps for the preprocessor pipeline 
-            nan_replacement_step = ("nan_replacement", FunctionTransformer(replace_na_with_nan))
+
             imputer_step = ("imputer",SimpleImputer(strategy="constant",fill_value= 0))
             scaler_step= ("scaler", RobustScaler())
 
             preprocessor = Pipeline(
                 steps = [
-                    nan_replacement_step,
-                    imputer_step,
-                    scaler_step
+                imputer_step,
+                scaler_step
                 ]
             )
 
